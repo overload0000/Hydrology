32d52314220d0fb0da13202e973ff7764a45f851
于骏浩 <shuimuqinghua@yujunhaodeMacBook-Pro.local>
2023-12-06 10:59:39 +0800

    feat: add pcp split and geo split

--- main.py
+++ main.py
@@ -8,12 +8,38 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
+def pcp_split_regression(df: pd.DataFrame, pcp_bin: int):
+    """
+    split the data by precipitation
+    """
+    gpt_data_by_pcp_groups = split_data_by_pcp(df, pcp_bin)
+
+    logger.info("getting extreme precipitation and run log linear regression")
+    for i, group in tqdm(
+        enumerate(gpt_data_by_pcp_groups), total=len(gpt_data_by_pcp_groups)
+    ):
+        extreme_pcps, avg_extreme_pcps = get_extreme_for_each_temp(group)
+        log_linear_regression(extreme_pcps, f"pcp_{i * pcp_bin}-{(i + 1) * pcp_bin}")
+
+
+def geo_split_regression(df: pd.DataFrame, lat_bin: int, lon_bin: int):
+    """
+    split the data by lat and lon
+    """
+    gpt_data_by_geo_groups = split_data_by_geo(df, lat_bin, lon_bin)
+
+    logger.info("getting extreme precipitation and run log linear regression")
+    for i, group in tqdm(
+        enumerate(gpt_data_by_geo_groups), total=len(gpt_data_by_geo_groups)
+    ):
+        extreme_pcps, avg_extreme_pcps = get_extreme_for_each_temp(group)
+        log_linear_regression(extreme_pcps, f"geo_{i}")
+
+
 if __name__ == "__main__":
-    precipitation = pd.read_pickle('data/pcp_data.pkl')
-    temperature = pd.read_pickle('data/temp_data.pkl')
+    logger.info("loading data")
+    gpt_data = pd.read_pickle("data/gpt_data.pkl")
 
-    # import pdb; pdb.set_trace()
-    pcp_geo = extract_geo_data(precipitation, "pcp")
-    temp_geo = extract_geo_data(temperature, "temp")
-
-    data = merge_data([pcp_geo, temp_geo])
+    logger.info("splitting data by precipitation and apply log linear regression")
+    pcp_split_regression(gpt_data, 200)
--- utils.py
+++ utils.py
@@ -3,13 +3,13 @@ import numpy as np
 import os
 import re
 import logging
-from rich.progress import track
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from typing import List
 from matplotlib import pyplot as plt
 from statsmodels.formula.api import ols
 
 logger = logging.getLogger(__name__)
+
 
 def check_dir(dir):
     """
@@ -18,25 +18,40 @@ def check_dir(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
+
 def extract_geo_data(
     df: pd.DataFrame,
     data_name: str,
 ):
+    """
+    extract geo data from the dataframe
+    it's OK you ignore this function
+    """
     logger.info(f"extracting {data_name} data")
-    new_columns = df.columns.to_series().apply(lambda x: re.findall(r'\d+', x)).apply(lambda x: tuple(x))
+    new_columns = (
+        df.columns.to_series()
+        .apply(lambda x: re.findall(r"\d+", x))
+        .apply(lambda x: tuple(x))
+    )
     df.columns = pd.MultiIndex.from_tuples(new_columns)
     df = df.reset_index().melt(id_vars="index").set_index("index")
     df = df.reset_index().rename(columns={"index": "time"})
-    df = df.rename(columns={"value": data_name, "variable_0": "LONG", "variable_1": "LAT"})
+    df = df.rename(
+        columns={"value": data_name, "variable_0": "LONG", "variable_1": "LAT"}
+    )
     return df
+
 
 def merge_data(
     dfs: List[pd.DataFrame],
 ):
+    """
+    merge the data
+    it's OK you ignore this function
+    """
     df = dfs[0]
-    logger.info(f"merging data")
-    # for i in track(range(1, len(dfs))):
-    for i in range(1, len(dfs)):
+    logger.info("merging data")
+    for i in trange(1, len(dfs), leave=None):
         current = dfs[i]
         df[current.columns[-1]] = current.iloc[:, -1]
     return df
@@ -50,27 +65,20 @@ def get_extreme_pcp(df: pd.DataFrame, threshold=0.95):
     return df.iloc[: int(df.shape[0] * (1 - threshold)), :]
 
 
-def split_data_by_pcp(
-    df: pd.DataFrame,
-    pcp_step: float
-):
+def split_data_by_pcp(df: pd.DataFrame, pcp_step: float):
     """
     split the data by precipitation
     """
     min_pcp = df["pcp"].min()
     max_pcp = df["pcp"].max()
-
+    logger.info("splitting data")
     groups = []
-    for i in track(np.arange(min_pcp, max_pcp, pcp_step)):
+    for i in trange(int((max_pcp - min_pcp) // pcp_step), leave=False):
         groups.append(df[(df["pcp"] >= i) & (df["pcp"] < i + pcp_step)])
-
     return groups
 
-def split_data_by_geo(
-    df: pd.DataFrame,
-    lon_step: float,
-    lat_step: float
-):
+
+def split_data_by_geo(df: pd.DataFrame, lon_step: float, lat_step: float):
     """
     split the data by longitude and latitude
     """
@@ -80,11 +88,23 @@ def split_data_by_geo(
     max_lat = df["LAT"].max()
 
     groups = []
-    for i in track(np.arange(min_lon, max_lon, lon_step)):
-        for j in track(np.arange(min_lat, max_lat, lat_step)):
-            groups.append(df[(df["LONG"] >= i) & (df["LONG"] < i + lon_step) & (df["LAT"] >= j) & (df["LAT"] < j + lat_step)])
+    logger.info("splitting data")
+    for i in trange(
+        int((max_lon - min_lon) // lon_step * (max_lat - min_lat) // lat_step),
+        leave=None,
+    ):
+        for j in trange(int((max_lon - min_lon) // lon_step), leave=None):
+            groups.append(
+                df[
+                    (df["LONG"] >= i)
+                    & (df["LONG"] < i + lon_step)
+                    & (df["LAT"] >= j)
+                    & (df["LAT"] < j + lat_step)
+                ]
+            )
 
     return groups
+
 
 def get_percentiles(df: pd.DataFrame):
     """
@@ -94,16 +114,17 @@ def get_percentiles(df: pd.DataFrame):
     df = df.sort_values(by="temp")
     temps = []
     pcps = []
-    for i in track(range(100)):
+
+    logger.info("getting percentiles")
+    for i in trange(100, leave=None):
         curr = df.iloc[int(length * i / 100) : int(length * (i + 1) / 100), :]
         temps.append(curr["temp"].mean())
         pcps.append(curr["pcp"].mean())
-
     return temps, pcps
 
 
 def get_extreme_for_each_temp(
-    df: pd.DataFrame, step=0.5, thresholds=[0.9, 0.95, 0.99], drop_threshold=1000
+    df: pd.DataFrame, step=0.5, thresholds=[0.9, 0.95, 0.99], drop_threshold=300
 ):
     """
     group the data by temperature with a given step
@@ -114,20 +135,25 @@ def get_extreme_for_each_temp(
     max_temp = df["temp"].max()
 
     groups = []
-    for i in track(np.arange(min_temp, max_temp, step)):
+
+    logger.info("grouping data by temperature with step {}".format(step))
+    for i in trange(int((max_temp - min_temp) // step), leave=None):
         groups.append(df[(df["temp"] >= i) & (df["temp"] < i + step)])
+    logger.info("dropping groups with less than {} samples".format(drop_threshold))
     groups = [group for group in groups if group.shape[0] >= drop_threshold]
 
     extreme_pcps = []
     avg_extreme_pcps = []
-    for threshold in track(thresholds):
+
+    logger.info("getting average extremes for thresholds {}".format(thresholds))
+    for threshold in tqdm(thresholds, leave=None):
         extreme_pcp = []
         avg_extreme_pcp = []
-        for group in track(groups):
+        for group in tqdm(groups, leave=None):
             extreme_pcp.append(get_extreme_pcp(group, threshold))
 
-        for i in track(len(extreme_pcp)):
-            avg_extreme_pcp.append(extreme_pcp[i].mean())
+        for i in tqdm(range(len(extreme_pcp)), leave=None):
+            avg_extreme_pcp.append(extreme_pcp[i][["pcp", "temp"]].mean())
 
         avg_extreme_pcps.append(pd.concat(avg_extreme_pcp))
 
@@ -173,7 +199,7 @@ def draw_percentiles(extreme_pcps: List[pd.DataFrame], thresholds: List[float]):
     temps = []
     pcps = []
 
-    for i in track(range(len(extreme_pcps))):
+    for i in trange(len(extreme_pcps), leave=None):
         temp, pcp = get_percentiles(extreme_pcps[i])
         temps.append(temp)
         pcps.append(pcp)
@@ -194,19 +220,31 @@ def draw_percentiles(extreme_pcps: List[pd.DataFrame], thresholds: List[float]):
     plt.savefig(os.path.join("pic", "extreme_pcp_percentile_log.png"))
 
 
-def log_linear_regression(extreme_pcps: List[pd.DataFrame], epsilon=1e-3):
-    for i in track(range(len(extreme_pcps))):
-        extreme_pcp = extreme_pcps[i]
-        extreme_pcp["log_pcp"] = np.log(extreme_pcp["pcp"] + epsilon)
+def log_linear_regression(extreme_pcps: List[pd.DataFrame], name: str, epsilon=1e-3):
+    """
+    run log linear regression
 
-        extreme_pcp = extreme_pcp[
-            extreme_pcp["temp"] > extreme_pcp["temp"].quantile(0.01)
-        ]
-        extreme_pcp = extreme_pcp[
-            extreme_pcp["temp"] < extreme_pcp["temp"].quantile(0.99)
-        ]
+    Args:
+        extreme_pcps: list of extreme precipitation for each threshold
+        name: name of the regression
+        epsilon: epsilon for log to avoid log(0)
+    """
+    logger.info("log regression")
+    with open(os.path.join("output", name + "log_regression.txt"), "w") as f:
+        f.write("log regression\n")
+        f.write("epsilon = {}\n".format(epsilon))
+        f.write("criterion = {}\n".format(name))
+        for i in trange(len(extreme_pcps), leave=None):
+            extreme_pcp = extreme_pcps[i]
+            extreme_pcp["log_pcp"] = np.log(extreme_pcp["pcp"] + epsilon)
 
-        model = ols("log_pcp ~ temp", data=extreme_pcp).fit()
+            extreme_pcp = extreme_pcp[
+                extreme_pcp["temp"] > extreme_pcp["temp"].quantile(0.01)
+            ]
+            extreme_pcp = extreme_pcp[
+                extreme_pcp["temp"] < extreme_pcp["temp"].quantile(0.99)
+            ]
 
-        with open(os.path.join("output", "log_regression.txt"), "w") as f:
+            model = ols("log_pcp ~ temp", data=extreme_pcp).fit()
+            f.write(f"threshold: {i}\n")
             f.write(model.summary().as_text())
